3ef7a80415fec913a14cd4653cde31bc84d93403
AndIgnacioOrellana <iorellana@andreani.com>
2026-06-24 19:06:30 -0300

    agregar carpeta docs

---
+++ docs/assets/MyLabel.stories-CThU4L4h.js
@@ -0,0 +1,17 @@
+import{i as e}from"./preload-helper-CT_b8DTk.js";import{t}from"./jsx-runtime-DqZldVDK.js";import{t as n}from"./compiler-runtime-BmFrWgUc.js";var r=e((()=>{})),i,a,o,s=e((()=>{i=n(),r(),a=t(),o=e=>{let t=(0,i.c)(6),{label:n,size:r,color:o,fontColor:s}=e,c=r===void 0?`normal`:r,l=o===void 0?`text-primary`:o,u=s===void 0?``:s,d=`${c} ${l}`,f;t[0]===u?f=t[1]:(f={color:u},t[0]=u,t[1]=f);let p;return t[2]!==n||t[3]!==d||t[4]!==f?(p=(0,a.jsx)(`div`,{className:d,style:f,children:n}),t[2]=n,t[3]=d,t[4]=f,t[5]=p):p=t[5],p},o.__docgenInfo={description:``,methods:[],displayName:`MyLabel`,props:{size:{defaultValue:{value:`"normal"`,computed:!1},required:!1},color:{defaultValue:{value:`"text-primary"`,computed:!1},required:!1},fontColor:{defaultValue:{value:`""`,computed:!1},required:!1}}}})),c,l,u,d,f;e((()=>{s(),c={title:`Example/MyLabel`,component:o,tags:[`autodocs`],parameters:{layout:`centered`},argTypes:{label:{description:`Text to display`,table:{type:{summary:`string`}},control:`text`},size:{description:`Size of the label`,table:{type:{summary:`normal | h1 | h2 | h3`}},control:`select`,options:[`normal`,`h1`,`h2`,`h3`]},color:{description:`Color of the label`,table:{type:{summary:`text-primary | text-secondary`}},control:`select`,options:[`text-primary`,`text-secondary`]},fontColor:{description:`Color of the font`,table:{type:{summary:`string`}},control:`color`}}},l={args:{label:`MyLabel`,color:`text-primary`,size:`h3`}},u={args:{label:`MyLabel`,color:`text-secondary`}},d={args:{label:`MyLabel`,fontColor:`#01f688`}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "MyLabel",
+    color: "text-primary",
+    size: "h3"
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "MyLabel",
+    color: "text-secondary"
+  }
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "MyLabel",
+    fontColor: "#01f688"
+  }
+}`,...d.parameters?.docs?.source}}},f=[`Primary`,`Secondary`,`Custom`]}))();export{d as Custom,l as Primary,u as Secondary,f as __namedExportsOrder,c as default};
